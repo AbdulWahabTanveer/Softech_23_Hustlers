66f2c1df764260f11b854d71a89259192233c0a4
Abdul Wahab <66131734+AbdulWahabTanveer@users.noreply.github.com>
2023-05-06 11:54:40 +0500

    added boiler plate code

---
+++ android/app/src/main/kotlin/com/hustlers/softech23/softech_hustlers/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hustlers.softech23.softech_hustlers
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Demo.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class Dummy extends StatelessWidget {
+  const Dummy({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
