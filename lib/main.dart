2d68542a0802cc373ff569fcac2f4b1be847cd3f
rehan <rehanjutt005@gmail.com>
2023-05-06 00:50:55 -0700

    setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:softech_hustlers/Demo.dart';
+import 'package:softech_hustlers/style/app_theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,11 +14,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
-      title: 'Softech Hustlers',
-      home: Dummy(),
+    //Set the fit size (Find your UI design, look at the dimensions of the device screen and fill it in,unit in dp)
+    return ScreenUtilInit(
+      designSize: const Size(392.727272, 825.4545),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context , child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'First Method',
+          // You can use the library anywhere in the app even in theme
+          theme: AppTheme.lightTheme,
+          home: child,
+        );
+      },
+      child: Dummy(),
     );
-  }
-}
+  }}
 
 
