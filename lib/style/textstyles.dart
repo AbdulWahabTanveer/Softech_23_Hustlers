788b17ecfe2d96a323c04b4fe04d752b0c9425d1
rehan <rehanjutt005@gmail.com>
2023-05-06 03:22:24 -0700

    setup

--- lib/style/textstyles.dart
+++ lib/style/textstyles.dart
@@ -3,3 +3,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 final TextStyle ts20W800= TextStyle(fontSize: 20.sp, fontWeight: FontWeight.w800 );
 final TextStyle ts16W400= TextStyle(fontSize: 16.sp, fontWeight: FontWeight.w400 );
+final TextStyle grey12W400 = TextStyle(fontWeight: FontWeight.w400, fontSize: 12.sp, color: Colors.grey[600]);
+final TextStyle ts14w400 = TextStyle(fontSize: 14.sp, fontWeight: FontWeight.w400,decoration: TextDecoration.underline, fontStyle: FontStyle.italic,);
+
--- lib/ui/authentication/login_screen.dart
+++ lib/ui/authentication/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+import 'package:get/get.dart';
 import 'package:nb_utils/nb_utils.dart';
 import 'package:softech_hustlers/global_widgets/busy_button.dart';
 import 'package:softech_hustlers/global_widgets/custom_text_field.dart';
@@ -20,19 +21,39 @@ class LoginScreen extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: kpHorizontalPadding),
+          padding: EdgeInsets.symmetric(horizontal: kpHorizontalPadding.w),
           child: Column(
             children: [
-              60.verticalSpace,
+              100.verticalSpace,
               Text('Hello Again!', style: ts20W800),
               20.verticalSpace,
               Text('Welcome Back, You Have Been\n Missed For A Long Time', style: ts16W400,textAlign: TextAlign.center,),
-              CustomTextField(controller: loginController.loginEmailCont, validator: loginController.emailValidation, label: "Email", suffix: Icon(Fontisto.email, size: 25.h,), hint: "Enter Email",),
+              60.verticalSpace,
+              CustomTextField(controller: loginController.loginEmailCont, validator: loginController.emailValidation, label: "Email", suffix: Icon(Icons.email, size: 25.h,), hint: "Enter email here",),
               20.verticalSpace,
-              CustomTextField(controller: loginController.loginPassCont, validator: loginController.emailValidation, label: "Password", suffix: Icon(Fontisto.locked, size: 25.h,),hint: "Enter Password"),
-              40.verticalSpace,
-              BusyButton(title: "Sign In", isBusy: loginController.)
-              
+              CustomTextField(controller: loginController.loginPassCont, validator: loginController.emailValidation, label: "Password", suffix: Icon(Icons.lock, size: 25.h,),hint: "Enter password here"),
+              20.verticalSpace,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text('Forgot password?', style: ts16W400.copyWith(color: Theme.of(context).primaryColor, fontWeight: FontWeight.w600, fontStyle: FontStyle.italic),),
+
+                ],
+              ),
+              70.verticalSpace,
+              Obx(() => BusyButton(title: "Sign In", isBusy: loginController.loading.value,onPressed: ()=> loginController.signIn(loginController.loginEmailCont.text, loginController.loginPassCont.text),),),
+              60.verticalSpace,
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text("Don't have an account? ", style: grey12W400.copyWith(fontSize: 16.sp),),
+                  Text('Sign Up', style: ts14w400.copyWith(color: Theme.of(context).primaryColor, fontWeight: FontWeight.w800, fontSize: 16.sp),)
+
+                ],
+              )
+
+
+
 
 
             ],
@@ -42,3 +63,4 @@ class LoginScreen extends StatelessWidget {
     );
   }
 }
+
