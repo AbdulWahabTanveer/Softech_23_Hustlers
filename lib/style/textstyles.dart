88b2a02cbec9a05b3e4698cc2e6a7095927e0c08
Abdul Wahab <66131734+AbdulWahabTanveer@users.noreply.github.com>
2023-05-06 15:03:12 +0500

    Update textstyles.dart

---
+++ lib/style/textstyles.dart
@@ -0,0 +1,49 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:softech_hustlers/style/colors.dart';
+
+final TextStyle appBarTextStyle = TextStyle(
+  fontSize: 20.sp,
+  color: Colors.white,
+  fontWeight: FontWeight.w700,
+);
+
+final TextStyle white18w700 = TextStyle(
+  fontSize: 18.sp,
+  color: Colors.white,
+  fontWeight: FontWeight.w700,
+);
+
+final TextStyle white18w500 = TextStyle(
+  fontSize: 18.sp,
+  color: Colors.white,
+  fontWeight: FontWeight.w500,
+);
+final TextStyle white14w500 = TextStyle(
+  fontSize: 14.sp,
+  color: Colors.white,
+  fontWeight: FontWeight.w500,
+);
+final TextStyle grey14w500 = TextStyle(
+  fontSize: 14.sp,
+  color: Colors.grey,
+  fontWeight: FontWeight.w500,
+);
+final TextStyle theme16w500 = TextStyle(
+  fontSize: 16.sp,
+  color: primaryColor,
+  fontWeight: FontWeight.w500,
+);
+
+final TextStyle theme18w500 = TextStyle(
+  fontSize: 18.sp,
+  color: primaryColor,
+  fontWeight: FontWeight.w500,
+);
+
+final TextStyle theme16w700 = TextStyle(
+  fontSize: 16.sp,
+  color: primaryColor,
+  fontWeight: FontWeight.w700,
+);
